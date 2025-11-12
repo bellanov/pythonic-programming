59eca51af36aca347d5c1fe6e8a5e80d9166cb02
charlestone <cjmayenga@aggies.ncat.edu>
2025-11-12 17:03:30 -0500

    Added data type examples

---
+++ data_types.py
@@ -0,0 +1,41 @@
+# Data Types in Python
+# integer
+multiplication = 5 * 2
+result1 = 10 #integer
+
+division  = 10 // 2 # // is floor division operator that returns an integer result
+result2 = 5 #integer
+2 + 3  # results in 5
+4 - 1  # results in 3   
+
+a = 10
+print("Integer:", a, "Type:", type(a))
+
+# float
+b = 10.5
+print("Float:", b, "Type:", type(b))
+
+multiplication = 5.1 * 2.0
+result3 = 10.2 #float
+
+division  = 10.0 / 4.0 # / is division operator that returns a float result
+result4 = 2.5 #float
+2.1 + 3.4  # results in 5.5
+4.5 - 1.2  # results in 3.3
+
+
+# string
+c = "Hello, Python!"
+print("String:", c, "Type:", type(c))
+
+# boolean
+d = True
+print("Boolean:", d, "Type:", type(d))
+
+e = False
+print("Boolean:", e, "Type:", type(e)),
+
+True + True  # results in 2
+False + True # results in 1
+False + False # results in 0
+# same to and, or, not operations
